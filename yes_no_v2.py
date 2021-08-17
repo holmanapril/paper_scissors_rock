521120ad54d2c54c8fbaf338490311e413ecc9c7
holman.april <holman.april@awatapu.school.nz>
2021-08-17 13:06:37 +1200

    Made the if and elif statements combined and more efficient

--- yes_no_v2.py
+++ yes_no_v2.py
@@ -3,9 +3,10 @@ while show_instructions.lower() != "xxx":
     # Ask User if they have played before
     yes_no = input("Have you played before?").strip().lower()
     if yes_no == "yes" or yes_no == "y":
-        print("You chose Yes")
+        yes_no = "yes"
+        print("Program Continues")
     elif yes_no == "no" or yes_no == "n":
-        print("You chose No")
+        yes_no = "no"
+        print("Display Instructions")
     else:
         print("Please enter Yes or No")
-        yes_no = input("Have you played before?").strip().lower()
