f0e1e802e571e1424036bd2c702501b783b6a466
holman.april <holman.april@awatapu.school.nz>
2021-08-17 12:43:54 +1200

    Just made it a loop for testing through possible string input

--- yes_no_v1.py
+++ yes_no_v1.py
@@ -1,13 +1,15 @@
-# Ask User if they have played before
-yes_no = input("Have you played before?").strip().lower()
-if yes_no == "yes":
-    print("You chose Yes")
-elif yes_no == "y":
-    print("You chose Yes")
-elif yes_no == "no":
-    print("You chose No")
-elif yes_no == "n":
-    print("You chose No")
-else:
-    print("Please enter Yes or No")
+show_instructions = ""
+while show_instructions.lower() != "xxx":
+    # Ask User if they have played before
     yes_no = input("Have you played before?").strip().lower()
+    if yes_no == "yes":
+        print("You chose Yes")
+    elif yes_no == "y":
+        print("You chose Yes")
+    elif yes_no == "no":
+        print("You chose No")
+    elif yes_no == "n":
+        print("You chose No")
+    else:
+        print("Please enter Yes or No")
+        yes_no = input("Have you played before?").strip().lower()
